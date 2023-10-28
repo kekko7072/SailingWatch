8d0fb2d9c4cc3476bc1bf221e2b77c794d459173
Francesco Vezzani <32552589+kekko7072@users.noreply.github.com>
2023-10-28 15:59:50 +0200

    Initial Commit

---
+++ SailingWatch Watch App/SailingWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SailingWatchApp.swift
+//  SailingWatch Watch App
+//
+//  Created by Francesco Vezzani on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SailingWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
